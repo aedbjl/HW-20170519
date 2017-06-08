ef2c38f3d51af36eae359e1611a59f1b1e626b89
HAHALHA <hiinetoeo@gmail.com>
2017-06-08 23:42:20 +0800

    test1

---
+++ HW/123.php
@@ -0,0 +1,28 @@
+<?php
+include 'sql.php';
+$pdo=new pdo($dsn,$user,$password,$opt);
+$id = $_REQUEST['id'];
+$sql="select account from member  where id = ?";
+$stmt=$pdo->prepare($sql);
+$stmt->execute([$id]);
+$goal = 0;
+$obj = $stmt->fetchObject();
+//$goal = $obj->account;
+$rs = rand(0, 10);
+if ($rs <= "$obj->account") {
+    $goal += 1;
+} else {
+    $goal += 0;
+}
+echo $goal ;
+//            while ($obj = $stmt->fetchObject()) {
+//
+//                $rs = rand(0, 10);
+//                if ($rs <= "$obj->account") {
+//                    $goal += 1;
+//                } else {
+//                    $goal += 0;
+//                }
+//
+//            }
+//            echo $goal;
