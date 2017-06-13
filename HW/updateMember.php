9eeda860a73b75735536ef90b34c89e83c28be29
aedbjl <hiinetoeo@gmail.com>
2017-06-13 16:26:32 +0800

    Create updateMember.php

--- HW/updateMember.php
+++ HW/updateMember.php
@@ -7,6 +7,6 @@ $password=$_GET['contact'];
 $realname=$_GET['speed'];
 $sql="update member set power='{$account}',contact='{$password}', speed ='{$realname}' where id={$id}";
 $db = @new mysqli('127.0.0.1',
-    'root','root','iii');
+    'root','root','061408');
 $db->query($sql);
 header("Location: 555.php");
